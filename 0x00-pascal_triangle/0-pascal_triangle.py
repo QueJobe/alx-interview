b15687b9bfae55f4071df69292cca31de4d3a0fa
Blessing <bqdithole22@live.com>
2024-08-10 23:19:56 +0200

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n:
+    """
+
+    if n <= 0:
+        return []
+
+    pascal = [[] for idx in range(n)]
+
+    for li in range(n):
+        for col in range(li+1):
+            if(col < li):
+                if(col == 0):
+                    pascal[li].append(1)
+                else:
+                    pascal[li].append(pascal[li-1][col] + pascal[li-1][col-1])
+            elif(col == li):
+                pascal[li].append(1)
+
+    return pascal
